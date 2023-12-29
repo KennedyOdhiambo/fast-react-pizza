3354e9fe7cbfdea62073266e5cdaa98ffd612fe9
kennedy <x.johnkennedy.x@gmail.com>
2023-12-29 19:46:38 +0300

    Styling components with Tailwind

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,17 +1,29 @@
+import PropTypes from "prop-types";
 import { formatCurrency } from "../../utils/helpers";
-function OrderItem({ item, isLoadingIngredients, ingredients }) {
+
+function OrderItem({ item }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
+    <li className="py-3">
+      <div className="flex items-center justify-between gap-4 text-sm">
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p>{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
 }
+
+OrderItem.propTypes = {
+  item: PropTypes.shape({
+    quantity: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    totalPrice: PropTypes.number.isRequired,
+  }).isRequired,
+  isLoadingIngredients: PropTypes.bool, // Optional boolean prop
+  ingredients: PropTypes.arrayOf(PropTypes.string), // Optional array of strings prop
+};
 
 export default OrderItem;
